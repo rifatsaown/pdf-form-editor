63629486302ec5d54ad9e94dcaa2e297239a6629
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-07-18 14:20:44 +0600

    chore: Add PDF form field retrieval functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+const { PDFDocument } = require("pdf-lib");
+const fs = require("fs");
+const path = require("path");
+
+const getFields = async () => {
+  const existingPdfBytes = fs.readFileSync(path.join(__dirname, "template.pdf"));
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+  const form = pdfDoc.getForm();
+  const fields = form.getFields();
+  return fields;
+};
+
+getFields().then(fields => {
+  fields.forEach(field => {
+    // console.log(field.getName());
+    // console.log(`${field.getName()}`);
+    console.log(`${field}`);
+  });
+}).catch(error => {
+  console.error(error);
+});
