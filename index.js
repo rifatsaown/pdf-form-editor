714fae6c9a91b3a530f548dff69ae8751ff7a3c9
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-07-30 00:08:27 +0600

    chore: Update PDF form fields with user input

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-const { PDFDocument } = require("pdf-lib");
-const fs = require("fs");
-const path = require("path");
+import { PDFDocument } from 'pdf-lib';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
 
 const getFields = async () => {
-  const existingPdfBytes = fs.readFileSync(
-    path.join(__dirname, "template.pdf")
+  const existingPdfBytes = readFileSync(
+    join(__dirname, 'template.pdf')
   );
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
-
   const form = pdfDoc.getForm();
-  const fields = form.getFields();
-  return fields;
+  form.getTextField('qbId').setText('1111');
+  form.getTextField('aideName').setText('John Doe');
+  form.getTextField('assignmentId').setText('2222');
+  form.getTextField('patientPhone1').setText('123-456-7890');
+  form.getTextField('patientPhone').setText('123-456-7890');
+  form.getTextField('startingDate').setText('01/01/2021');
+  form.getTextField('patientFirstName').setText('Jane');
+  form.getTextField('patientLastName').setText('Doe');
+  form.getTextField('patientAddress').setText('123 Main St');
+  form.getTextField('patientDOB').setText('01/01/2001');
+  form.getDropdown('patientGender').select('Male');
+  form.getTextField('Monday').setText('');
+  form.getTextField('Tuesday').setText('');
+  form.getTextField('Wednesday').setText('');
+  form.getTextField('Thursday').setText('');
+  form.getTextField('Friday').setText('');
+  form.getTextField('Saturday').setText('');
+  form.getTextField('Sunday').setText('8:00 AM - 5:00 PM');
+  form.getTextField('specialNotes').setText('Special notes here ... ');
+
+  const pdfBytes = await pdfDoc.save();
+  writeFileSync(join(__dirname, 'output.pdf'), pdfBytes);
 };
 
 getFields()
-  .then((fields) => {
-    fields.forEach((field) => {
-      console.log(field.getName());
-      // console.log(`${field.getName()}`);
-      // console.log(`${field}`);
-    });
+  .then(() => {
+    console.log('Done');
   })
-  .catch((error) => {
-    console.error(error);
+  .catch((err) => {
+    console.error(err);
   });
